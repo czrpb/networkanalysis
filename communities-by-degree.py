9cdfbfeed875d620f1773bcfd8fd5dd913fe3b52
Quentin Crain <czrpb00@gmail.com>
2024-01-19 08:08:31 -0800

    updates: 20240119-080831

--- communities-by-degree.py
+++ communities-by-degree.py
@@ -13,13 +13,30 @@ def get_random_communities():
     c2 = ns[len(ns)//2:]
     return c1, c2
 
+def get_largest_neighbor(n):
+    largest_neighbor, *neighbors = list(nx.neighbors(main, n))
+    for neighbor in neighbors:
+        if nx.degree(main, largest_neighbor) < nx.degree(main, neighbor):
+            largest_neighbor = neighbor
+    return largest_neighbor
+
 def get_communities():
+    communities = dict([(n, list(nx.neighbors(main, n))) for n in nodes])
+    print(communities)
+
     ns = sorted([(nx.degree(main, n), n) for n in nodes])
     print(ns)
+    for _, n in ns[:-2]:
+        largest_neighbor = get_largest_neighbor(n)
+        communities[largest_neighbor].append(n)
+        del communities[n]
+
+    print(communities)
+    return [[k]+v for k, v in communities.items()]
 
 community_1, community_2 = get_random_communities()
 
-get_communities()
+community_1, community_2 = get_communities()
 
 pos = nx.spring_layout(main)
 nx.draw_networkx_nodes(main, pos, nodelist=community_1, node_shape="s", node_color="red")
